9a2d33a1cc9883c2ef07c6344f013a75a2421e28
Sidnei Silva <tjsidnei@gmail.com>
2022-03-24 21:44:05 +0000

    7th commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,17 @@
     <div class="block">
       <div class="container">
         <img class="calc-img" src="./../assets/calc.png" alt="calculadora">
-        <div>Eu também sofria com esse problema, e agora?</div>
+        <div class="info">
+          <div class="title-home">Eu também sofria com esse problema, e agora?</div>
+          <div class="my-text">Se você também é daqueles que tem imensa dificuldade de fazer contas de cabeça, seus problemas acabaram. Esta calculadora feita em Vue faz as contas para você.<br><br>Escreva as operações sequencialmente em uma única fila que ela te dá o resultado. Não deixe de experimentar.<br><br>Sua vida será facilitada como nunca imaginaste antes.</div>
+          <div class="container-home">
+            <div>&nbsp;</div>
+            <div class="container-info">
+              <div class="indicator-text">Acesse aqui</div>
+              <calculator :size="60" class="calculator-icon" @click="$router.push('/calc')" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -31,11 +41,15 @@
 </template>
 
 <script>
+import Calculator from 'vue-material-design-icons/Calculator.vue';
 import { mapState } from 'vuex'
 // @ is an alias to /src
 export default {
   computed: {
     ...mapState(['user', 'logged'])
+  },
+  components: {
+    Calculator
   }
 }
 </script>
